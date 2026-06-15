a88f9d5bc7978b9e32520aa78d30fd382c896b5a
Apurva <apurvarai45@gmail.com>
2026-06-15 21:25:38 +0530

    Updated Day3_Assignment

---
+++ Day3_Assignment/q2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main ()
+{
+    int start, end, i, j, isPrime;
+    printf("Enter starting number");
+    scanf("%d", &start);
+    printf("Enter ending number");
+    scanf("%d", &end);
+    printf("Prime numbers betweem %d and %d are\n", start, end);
+    for(i = start; i <= end; i++)
+    {
+        if(i <2)
+        {
+            continue;
+        }
+        isPrime = 1;
+        for(j = 2; j <= i / 2; j++)
+        {
+            if(i % j == 0)
+            {
+                isPrime = 0;
+                break;
+            }
+        } 
+        if(isPrime == 1)
+        {
+            printf("%d", i);
+        }
+    }
+    return 0;
+}
